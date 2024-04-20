edb4a714cc3959c813ec27d66276106602a36351
fansaiyan <irfanmuslimsaputra@gmail.com>
2024-04-20 21:08:24 +0700

    test api

--- rmi_survey/controllers/controllers.py
+++ rmi_survey/controllers/controllers.py
@@ -1,8 +1,8 @@
 from odoo import http
 from odoo.http import request
 from odoo.exceptions import AccessDenied
-
-
+from odoo.http import request, Response
+import json
 class CustomAPIController(http.Controller):
 
     @http.route('/custom_api/login', website=False, auth='public', type="http", csrf=False, methods=['POST'])
@@ -18,3 +18,16 @@ class CustomAPIController(http.Controller):
             raise AccessDenied()
 
         return http.Response('Login successful! User ID: {}'.format(uid))
+
+    @http.route('/api/rmi_param_group', website=False, auth='public', type="http", csrf=False, methods=['GET'])
+    def _get_pajak(self, **kw):
+        row_list = []
+        rows = request.env['rmi.param_group'].sudo().search([])[0]
+        if len(rows) > 0:
+            for row in rows:
+                row_list.append({
+                    'name': row.name
+                })
+        headers = {'Content-Type': 'application/json'}
+        body = {'results': {'code': 200, 'message': 'OK', 'data': row_list}}
+        return Response(json.dumps(body), headers=headers)
