6d7d5dae4e87bb5942d9d9705d7de5155ea34a51
fansaiyan <irfanmuslimsaputra@gmail.com>
2024-04-20 23:29:10 +0700

    some api report

--- rmi_survey/controllers/controllers.py
+++ rmi_survey/controllers/controllers.py
@@ -1,6 +1,7 @@
 from odoo import http
 from odoo.http import request, Response
 import json
+import io
 class CustomAPIController(http.Controller):
     @http.route('/web/session/authenticate', type='json', auth="none")
     def authenticate(self, db, login, password, base_location=None):
@@ -8,7 +9,7 @@ class CustomAPIController(http.Controller):
         return request.env['ir.http'].session_info()
 
     @http.route('/api/survey-list', website=False, auth='public', type="http", csrf=False, methods=['GET'])
-    def _get_pajak(self, **kw):
+    def _get_survey_list(self, **kwargs):
         data = []
         query = """
             select
@@ -27,3 +28,170 @@ class CustomAPIController(http.Controller):
         headers = {'Content-Type': 'application/json'}
         body = {'status': 200, 'message': 'OK', 'data': data}
         return Response(json.dumps(body), headers=headers)
+    @http.route('/api/adjust-aspek-dimensi', website=False, auth='public', type="http", csrf=False, methods=['GET'])
+    def _adjust_aspek_dimensi(self, **kwargs):
+        survey_id = kwargs.get('survey_id', None)
+        data = []
+        body = {}
+        statusCode = 200
+        headers = {'Content-Type': 'application/json'}
+        if not survey_id:
+            statusCode = 400
+            body = {
+                'status': False,
+                'message': 'Required parameter "survey_id" is missing'
+            }
+            return Response(json.dumps(body), headers=headers, status=statusCode)
+        try:
+            query = """
+                      select
+                        ROW_NUMBER() OVER () AS no,
+                        h.name as company,
+                        a.question_id as question_id,
+                        i.name as dimensi,
+                        j.name as subdimensi,
+                        (b.title->>'en_US')::varchar AS parameterName,
+                        avg((e.value->>'en_US')::int) as avgvalue,
+                        min((e.value->>'en_US')::int) as minvalue,
+                        max((e.value->>'en_US')::int) as maxvalue,
+                        max((e.value->>'en_US')::int) - min((e.value->>'en_US')::int) as rangevalue
+                        from survey_user_input_line as a
+                        left join survey_question as b on a.question_id = b.id
+                        left join survey_user_input as c on c.id = a.user_input_id
+                        left join res_partner as d on d.id = c.partner_id
+                        left join survey_question_answer as e on e.id = a.suggested_answer_id
+                        left join res_users as f on f.partner_id = d.id
+                        left join hr_employee as g on g.user_id = f.id
+                        left join res_company as h on h.id = g.company_id
+                        left join rmi_param_dimensi as i on i.id = b.dimensi_names
+                        left join rmi_param_group as j on j.id = b.sub_dimensi_names
+                    where a.survey_id = {} and c.state = 'done'
+                    GROUP BY a.question_id, parameterName, company, i.name, j.name
+                    ORDER BY a.question_id ASC
+                   """.format(survey_id)
+            http.request.env.cr.execute(query)
+            fetched_data = http.request.env.cr.fetchall()
+            column_names = [desc[0] for desc in http.request.env.cr.description]
+            for row in fetched_data:
+                row_dict = dict(zip(column_names, row))
+                data.append(row_dict)
+            body = {'status': True, 'message': 'OK', 'data': data}
+            statusCode = 200
+        except Exception as e:
+            errorMsg = f"An error occurred: {e}"
+            body = {
+                'status': False,
+                'message': errorMsg,
+                'execution_time': '0s'
+            }
+            statusCode = 500
+        return Response(json.dumps(body), headers=headers, status=statusCode)
+
+    @http.route('/api/adjust-aspek-dimensi-detail', website=False, auth='public', type="http", csrf=False, methods=['GET'])
+    def _adjust_aspek_dimensi_detail(self, **kwargs):
+        survey_id = kwargs.get('survey_id', None)
+        question_id = kwargs.get('question_id', None)
+        data = []
+        body = {}
+        statusCode = 200
+        headers = {'Content-Type': 'application/json'}
+        if not survey_id or not question_id:
+            statusCode = 400
+            body = {
+                'status': False,
+                'message': 'Required parameter is missing'
+            }
+            return Response(json.dumps(body), headers=headers, status=statusCode)
+        try:
+            query = """
+                  select
+                    ROW_NUMBER() OVER () AS No,
+                    a.question_id,
+                    h.name as company,
+                    (b.title->>'en_US')::varchar AS parameterName,
+                    d.name as user,
+                    i.name as Department,
+                    (e.value->>'en_US')::int AS value
+                    from survey_user_input_line as a
+                    left join survey_question as b on a.question_id = b.id
+                    left join survey_user_input as c on c.id = a.user_input_id
+                    left join res_partner as d on d.id = c.partner_id
+                    left join survey_question_answer as e on e.id = a.suggested_answer_id
+                    left join res_users as f on f.partner_id = d.id
+                    left join hr_employee as g on g.user_id = f.id
+                    left join res_company as h on h.id = g.company_id
+                    left join hr_department as i on i.id = g.department_id
+                where a.survey_id = {} and c.state = 'done' and a.question_id = {}
+               """.format(survey_id, question_id)
+            http.request.env.cr.execute(query)
+            fetched_data = http.request.env.cr.fetchall()
+            column_names = [desc[0] for desc in http.request.env.cr.description]
+            for row in fetched_data:
+                row_dict = dict(zip(column_names, row))
+                data.append(row_dict)
+            body = {'status': True, 'message': 'OK', 'data': data}
+            statusCode = 200
+        except Exception as e:
+            errorMsg = f"An error occurred: {e}"
+            body = {
+                'status': False,
+                'message': errorMsg,
+                'execution_time': '0s'
+            }
+            statusCode = 500
+        return Response(json.dumps(body), headers=headers, status=statusCode)
+    @http.route('/api/aspek-dimensi-corporate', website=False, auth='public', type="http", csrf=False, methods=['GET'])
+    def _aspek_dimensi_corporate(self, **kwargs):
+        survey_id = kwargs.get('survey_id', None)
+        data = []
+        body = {}
+        statusCode = 200
+        headers = {'Content-Type': 'application/json'}
+        if not survey_id:
+            statusCode = 400
+            body = {
+                'status': False,
+                'message': 'Required parameter "survey_id" is missing'
+            }
+            return Response(json.dumps(body), headers=headers, status=statusCode)
+        try:
+            query = """
+                      select
+                        CASE
+                            WHEN f.name = 'Budaya dan Kapabilitas Risiko' THEN '1 s.d. 3'
+                            WHEN f.name = 'Organisasi dan Tata Kelola Risiko' THEN '4 s.d. 19'
+                            WHEN f.name = 'Kerangka Risiko dan Kepatuhan' THEN '20 s.d. 33'
+                            WHEN f.name = 'Proses dan Kontrol Risiko' THEN '34 s.d. 39'
+                            ELSE '40 s.d. 42'
+                        END as parameter,
+                        ROW_NUMBER() OVER () AS dimensi,
+                        f.name as deskripsi,
+                        TRUNC(avg((e.value->>'en_US')::int) * 10) / 10.0 AS skordimensi
+                        from survey_user_input_line as a
+                        left join survey_question as b on a.question_id = b.id
+                        left join survey_user_input as c on c.id = a.user_input_id
+                        left join res_partner as d on d.id = c.partner_id
+                        left join survey_question_answer as e on e.id = a.suggested_answer_id
+                        left join rmi_param_dimensi as f on f.id = b.dimensi_names
+                        left join rmi_param_group as g on g.id = b.sub_dimensi_names
+                    where a.survey_id = {}
+                    group by f.name, f.id
+                    order by f.id asc
+                   """.format(survey_id)
+            http.request.env.cr.execute(query)
+            fetched_data = http.request.env.cr.fetchall()
+            column_names = [desc[0] for desc in http.request.env.cr.description]
+            for row in fetched_data:
+                row_dict = dict(zip(column_names, row))
+                data.append(row_dict)
+            body = {'status': True, 'message': 'OK', 'data': data}
+            statusCode = 200
+        except Exception as e:
+            errorMsg = f"An error occurred: {e}"
+            body = {
+                'status': False,
+                'message': errorMsg,
+                'execution_time': '0s'
+            }
+            statusCode = 500
+        return Response(json.dumps(body), headers=headers, status=statusCode)
