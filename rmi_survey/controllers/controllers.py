d2dde83f21edcca1dd1a420dce89663fa8159ffd
fansaiyan <irfanmuslimsaputra@gmail.com>
2024-04-20 22:07:08 +0700

    api authenticate

--- rmi_survey/controllers/controllers.py
+++ rmi_survey/controllers/controllers.py
@@ -31,3 +31,8 @@ class CustomAPIController(http.Controller):
         headers = {'Content-Type': 'application/json'}
         body = {'results': {'code': 200, 'message': 'OK', 'data': row_list}}
         return Response(json.dumps(body), headers=headers)
+
+    @http.route('/web/session/authenticate', type='json', auth="none")
+    def authenticate(self, db, login, password, base_location=None):
+        request.session.authenticate(db, login, password)
+        return request.env['ir.http'].session_info()
